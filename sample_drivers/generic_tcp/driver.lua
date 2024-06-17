58d1354c776f7af0d3288c7491e437cdd5c0e128
William Walsh <wwalsh@control4.com>
2024-06-17 11:45:52 +0100

    Generic TCP 2 - Use handlers.lua process for callbacks

--- sample_drivers/generic_tcp/driver.lua
+++ sample_drivers/generic_tcp/driver.lua
@@ -1,13 +1,11 @@
--- Copyright 2020 Wirepath Home Systems, LLC. All rights reserved.
+-- Copyright 2024 Snap One, LLC. All rights reserved.
 
-function Print (data)
-	if (type (data) == 'table') then
-		for k, v in pairs (data) do print (k, v) end
-	elseif (type (data) ~= 'nil') then
-		print (type (data), data)
-	else
-		print ('nil value')
-	end
+require ('drivers-common-public.global.handlers')
+require ('drivers-common-public.global.lib')
+require ('drivers-common-public.global.timer')
+
+do
+	DEFAULT_NETBINDING = 6001
 end
 
 function OnDriverLateInit ()
@@ -16,98 +14,48 @@ function OnDriverLateInit ()
 	end
 end
 
-function OnPropertyChanged (strProperty)
-	local value = Properties [strProperty]
-	if (value == nil) then
-		value = ''
-	end
+function OPC.Server_Address (value)
+	SERVER_ADDRESS = value
+end
 
-	if (strProperty == 'Server Address') then
-		SERVER_ADDRESS = value
+function OPC.Server_Port (value)
+	SERVER_PORT = tonumber (value)
+end
 
-	elseif (strProperty == 'Server Port') then
-		SERVER_PORT = tonumber (value)
+function OPC.Connection_Type (value)
+	CONNECTION_TYPE = value
+end
 
-	elseif (strProperty == 'Connection Type') then
-		CONNECTION_TYPE = value
+function OPC.Use_SSL (value)
+	USE_SSL = (value == 'Yes')
+end
 
-	elseif (strProperty == 'Use SSL') then
-		USE_SSL = (value == 'Yes')
-
-	elseif (strProperty == 'Open Connection') then
-		if (value == 'Open') then
-			OpenConnection ()
-
-		elseif (value == 'Close') then
-			CloseConnection ()
-		end
-
-	elseif (strProperty == 'Send Data') then
-		SendData (value)
+function OPC.Open_Connection (value)
+	if (value == 'Open') then
+		OpenConnection ()
+	elseif (value == 'Close') then
+		CloseConnection ()
 	end
 end
 
-function ExecuteCommand (strCommand, tParams)
-	tParams = tParams or {}
+function OPC.Send_Data (value)
+	SendData (value)
+end
 
-	local output = {'--- ExecuteCommand', strCommand, '----PARAMS----'}
-	for k,v in pairs (tParams) do table.insert (output, tostring (k) .. ' = ' .. tostring (v)) end
-	table.insert (output, '---')
-	output = table.concat (output, '\r\n')
-	dbg (output)
-
-	if (strCommand == 'LUA_ACTION') then
-		if (tParams.ACTION) then
-			strCommand = tParams.ACTION
-			tParams.ACTION = nil
-		end
-	end
-
-	if (strCommand == 'Send Data') then
-		SendData (tParams.DATA)
-	end
+function EC.Send_Data (tParams)
+	SendData (tParams.DATA)
 end
 
 function OpenConnection ()
 	if (USE_SSL) then
 		print ('Creating SSL binding to ' .. SERVER_ADDRESS)
-		C4:CreateNetworkConnection (6001, SERVER_ADDRESS, 'SSL')
+		C4:CreateNetworkConnection (DEFAULT_NETBINDING, SERVER_ADDRESS, 'SSL')
 	else
 		print ('Creating standard binding to ' .. SERVER_ADDRESS)
-		C4:CreateNetworkConnection (6001, SERVER_ADDRESS)
+		C4:CreateNetworkConnection (DEFAULT_NETBINDING, SERVER_ADDRESS)
 	end
-end
 
-function CloseConnection ()
-	C4:NetDisconnect (6001, SERVER_PORT)
-end
-
-function SendData (data)
-	data = string.gsub (data, '\\r', '\r')
-	data = string.gsub (data, '\\n', '\n')
-	if (CONNECTED) then
-		C4:SendToNetwork (6001, SERVER_PORT, data)
-		print (' >>> ', data)
-	end
-end
-
-function OnNetworkBindingChanged (idBinding, bIsBound)
-	print (idBinding, bIsBound)
-	if (idBinding == 6001) then
-		if (bIsBound) then
-			print ('Opening connection to ' .. SERVER_PORT .. ' of type ' .. CONNECTION_TYPE)
-			if (CONNECTION_TYPE == 'TCP') then
-				C4:NetConnect (6001, SERVER_PORT)
-			else
-				C4:NetConnect (6001, SERVER_PORT, CONNECTION_TYPE)
-			end
-		end
-	end
-end
-
-function OnConnectionStatusChanged (idBinding, nPort, strStatus)
-	print (idBinding, nPort, strStatus)
-	if (idBinding == 6001) then
+	OCS [DEFAULT_NETBINDING] = function (idBinding, nPort, strStatus)
 		if (nPort == SERVER_PORT) then
 			CONNECTED = (strStatus == 'ONLINE')
 			if (CONNECTED) then
@@ -117,16 +65,29 @@ function OnConnectionStatusChanged (idBinding, nPort, strStatus)
 			end
 		end
 	end
-end
 
-function ReceivedFromNetwork (idBinding, nPort, strData)
-	print (' <<< ', idBinding, nPort, strData)
-	if (idBinding == 6001) then
-		if (nPort == SERVER_PORT) then
-			C4:UpdateProperty ('Server Response', strData)
-
+	RFN [DEFAULT_NETBINDING] = function (idBinding, nPort, strData)
+		print (' <<< ', idBinding, nPort, strData)
+		if (idBinding == DEFAULT_NETBINDING) then
+			if (nPort == SERVER_PORT) then
+				C4:UpdateProperty ('Server Response', strData)
+			end
 		end
 	end
+
+	print ('Opening connection to ' .. SERVER_PORT .. ' of type ' .. CONNECTION_TYPE)
+	C4:NetConnect (DEFAULT_NETBINDING, SERVER_PORT, CONNECTION_TYPE)
 end
 
+function CloseConnection ()
+	C4:NetDisconnect (DEFAULT_NETBINDING, SERVER_PORT)
+end
 
+function SendData (data)
+	data = string.gsub (data, '\\r', '\r')
+	data = string.gsub (data, '\\n', '\n')
+	if (CONNECTED) then
+		C4:SendToNetwork (DEFAULT_NETBINDING, SERVER_PORT, data)
+		print (' >>> ', data)
+	end
+end
